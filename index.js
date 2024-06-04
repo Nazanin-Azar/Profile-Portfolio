a100fcf57dd372bbfe60f75248d75e61be7890d8
Nazanin Azar <dev.nazaninazar@gmail.com>
2024-06-04 15:18:07 +0330

    give some js code for header

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById('about').addEventListener('click', function() {
+    document.querySelector('.about__container').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('experience').addEventListener('click', function() {
+    document.querySelector('.experience__container').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('skill').addEventListener('click', function() {
+    document.querySelector('.skill__container').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('portfolio').addEventListener('click', function() {
+    document.querySelector('.portfolio__container').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact').addEventListener('click', function() {
+    document.querySelector('.contact__container').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('resume__button').addEventListener('click', function() {
+    window.open('resume.html', '_self');
+})
